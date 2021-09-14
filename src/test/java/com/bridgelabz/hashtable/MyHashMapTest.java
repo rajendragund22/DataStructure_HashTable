df52d4e5c63ade8f008684526b6f6ccf4c2c97b1
RajendraGund <rajendragund2015@gmail.com>
2021-09-15 01:26:24 +0530

    Added : Program for the find the frequency of word in large paragrap

--- src/test/java/com/bridgelabz/hashtable/MyHashMapTest.java
+++ src/test/java/com/bridgelabz/hashtable/MyHashMapTest.java
@@ -2,7 +2,6 @@ package com.bridgelabz.hashtable;
 
 import junit.framework.TestCase;
 import org.junit.Test;
-
 public class MyHashMapTest {
     private TestCase Assertions;
 
@@ -23,5 +22,25 @@ public class MyHashMapTest {
         int frequency = myHashMap.get("to");
         System.out.println(myHashMap);
         Assertions.assertEquals(2, frequency);
+    }
+
+    @Test
+    public void givenASentence_whenWordsAreAddedToList_shouldReturnParanoidWordFrequency() {
+        String sentence = "Paranoids are not paranoid because they are paranoid but " +
+                "because they keep putting themselves deliberately " +
+                "into paranoid avoidable situations";
+        MyHashMapParanoid<String, Integer> myLinkedHashMap = new MyHashMapParanoid<>();
+        String[] words = sentence.toLowerCase().split(" ");
+        for (String word : words) {
+            Integer value = myLinkedHashMap.get(word);
+            if (value == null)
+                value = 1;
+            else
+                value += 1;
+            myLinkedHashMap.add(word, value);
+        }
+        System.out.println(myLinkedHashMap);
+        int frequency = myLinkedHashMap.get("paranoid");
+        Assertions.assertEquals(3, frequency);
     }
 }
