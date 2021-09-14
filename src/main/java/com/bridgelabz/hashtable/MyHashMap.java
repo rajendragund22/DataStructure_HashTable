a21215d801aeb09449fce3daae49bba86903b557
RajendraGund <rajendragund2015@gmail.com>
2021-09-15 01:04:42 +0530

    Added : welcome message for the user

---
+++ src/main/java/com/bridgelabz/hashtable/MyHashMap.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.hashtable;
+
+public class MyHashMap {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Hash table implementation");
+    }
+
+}
